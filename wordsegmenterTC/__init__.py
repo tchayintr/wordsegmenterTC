b6c85cbc12863131fd86f1ee4baa62c3e29ccc0b
tchayintr <t.chayintr@icloud.com>
2017-10-25 17:51:42 +0700

    v0.1

--- wordsegmenterTC/__init__.py
+++ wordsegmenterTC/__init__.py
@@ -2,15 +2,15 @@ import PyICU
 from postaggerTC import Tagger
 
 SEPARATER = " "
-def wordsegmeter(text):
+class Segmenter:
 
-    def isThai(chr):
+    def isThai(self, chr):
         cVal = ord(chr)
         if (cVal >= 3584 and cVal <= 3711):
             return True
         return False
 
-    def segmenter(text):
+    def segment(self, text):
         bd = PyICU.BreakIterator.createWordInstance(PyICU.Locale("th"))
         bd.setText(text)
         lastPos = bd.first()
@@ -19,15 +19,16 @@ def wordsegmeter(text):
             while(True):
                 currentPos = next(bd)
                 retText += text[lastPos:currentPos]
-                if (isThai(text[currentPos - 1])):
+                if (self.isThai(text[currentPos - 1])):
                     if (currentPos < len(text)):
-                        if (isThai(text[currentPos])):
+                        if (self.isThai(text[currentPos])):
                             # Separater
                             retText += SEPARATER
                 lastPos = currentPos
         except StopIteration:
             pass
         return retText
-    return segmenter(text)
+
+
 
 
